ce13ef6cd58af889a6c754a058df3e8d70636eb9
agmcoding <antonioibmachado@gmail.com>
2023-01-25 23:37:19 -0300

    Solved challenge MaxConsecutiveOnes

---
+++ ProjectJavaCodeChallenges/src/packageMaxConsecutiveOnes/Main.java
@@ -0,0 +1,57 @@
+package packageMaxConsecutiveOnes;
+
+public class Main {
+
+//	Challenge:
+//	
+//	Given a binary array nums, return the maximum number of consecutive 1's in the array.
+//				 
+//	Example 1:
+//
+//	Input: nums = [1,1,0,1,1,1]
+//	Output: 3
+//	Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+//		
+//	Example 2:
+//
+//	Input: nums = [1,0,1,1,0,1]
+//	Output: 2
+//				 
+//	Constraints:
+//
+//	1 <= nums.length <= 105
+//	nums[i] is either 0 or 1.
+	
+	public static void main(String[] args) {
+
+		int[] nums = {1,0,1,1,0,1};
+		
+		System.out.println(findMaxConsecutiveOnes(nums));
+		
+		/* Output Result:
+		 *
+		 * 2
+		 * 
+		 */
+		
+	}
+
+	static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxConsecutiveOnes = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if ( nums[i] != 1) {
+                count = 0;
+            }
+            else {
+                count++;
+                if (count > maxConsecutiveOnes) {
+                    maxConsecutiveOnes = count;
+                }
+            }
+        }   
+        return maxConsecutiveOnes;
+    }
+	
+}
