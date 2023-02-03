b193b1cebeed7b0c362cda2692b8211630ce45d0
agmcoding <antonioibmachado@gmail.com>
2023-02-02 22:33:58 -0300

    Added another solution for challenge MaxConsecutiveOnes

--- ProjectJavaCodeChallenges/src/packageMaxConsecutiveOnes/Main.java
+++ ProjectJavaCodeChallenges/src/packageMaxConsecutiveOnes/Main.java
@@ -27,9 +27,13 @@ public class Main {
 		int[] nums = {1,0,1,1,0,1};
 		
 		System.out.println(findMaxConsecutiveOnes(nums));
+		System.out.println("Result with a method with Ternary Operator:");
+		System.out.println(findMaxConsecutiveOnesWithTernaryOperator(nums));
 		
 		/* Output Result:
 		 *
+		 * 2
+		 * Result with a method with Ternary Operator:
 		 * 2
 		 * 
 		 */
@@ -53,5 +57,20 @@ public class Main {
         }   
         return maxConsecutiveOnes;
     }
+	
+	
+	/* Using Ternary Operator and max() method from Math class */
+	
+	static int findMaxConsecutiveOnesWithTernaryOperator(int[] nums) {
+		int count = 0;
+		int maxConsecutiveOnes = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			count = nums[i] == 1 ? ++count : 0;
+			maxConsecutiveOnes = Math.max(count, maxConsecutiveOnes);
+		}
+		
+		return maxConsecutiveOnes;
+	}
 	
 }
