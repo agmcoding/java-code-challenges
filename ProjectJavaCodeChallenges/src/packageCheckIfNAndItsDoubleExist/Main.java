619068dd5e209cafd427477cc84a0c2916e15251
agmcoding <antonioibmachado@gmail.com>
2023-02-08 21:43:37 -0300

    Added HashSet implementation in challenge CheckIfNAndItsDoubleExist

--- ProjectJavaCodeChallenges/src/packageCheckIfNAndItsDoubleExist/Main.java
+++ ProjectJavaCodeChallenges/src/packageCheckIfNAndItsDoubleExist/Main.java
@@ -1,5 +1,8 @@
 package packageCheckIfNAndItsDoubleExist;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
 //	Challenge:
@@ -32,26 +35,67 @@ public class Main {
 		
 		int[] array = {10,2,5,3};
 		
-		System.out.println(checkIfExist(array));
+		System.out.println("With HashSet: " + checkIfExistWithHashSet(array));
+		System.out.println("With two for loops: " + checkIfExistWith2ForLoops(array));
 		
-		/* Output Result:
-		 *
-		 * true
-		 *
+		/**
+		 * Output Result:
+		 * 
+		 * With HashSet: true
+		 * With two for loops: true
 		 */
 		
 	}
 
-	static boolean checkIfExist(int[] arr) {
+	/**
+	 * With HashSet, operations as add() and contains() have O(1) time complexity.
+	 * Also, since we don't want to compare equal values, Set implementation will just add unique elements.
+	 * 
+	 * Information source about Set contains() method time complexity:
+	 * Link: https://stackoverflow.com/questions/25247854/what-is-the-time-complexity-performance-of-hashset-contains-in-java
+	 */
+	static boolean checkIfExistWithHashSet(int[] array) {
 		
-		if (arr == null || arr.length == 0) {
+		if (array == null || array.length == 0) {
 			return false;
 		}
 		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				if (i == j) continue;
-				if (arr[i] == 2 * arr[j]) {
+		Set<Integer> setArray = new HashSet<>();
+		
+		/** 
+		 * Instead of using IntStream of int array, like:
+		 * 
+		 * IntStream.of(array).boxed().collect(Collectors.toSet());
+		 * 
+		 * or like:
+		 * 
+		 * Arrays.stream(array).boxed().collect(Collectors.toSet());
+		 * 
+		 * , we can just add the elements one by one until we find a value corresponded to
+		 * a double or a half of an element that we added.
+		 */
+		
+		for (int i : array) {
+			if (setArray.contains(2 * i) || i % 2 == 0 && setArray.contains(i / 2))
+				return true;
+			setArray.add(i); /* If not, we just need to add the element in the HashSet to be compared next time. */
+		}
+		
+		return false;
+}
+
+
+
+	static boolean checkIfExistWith2ForLoops(int[] array) {
+		
+		if (array == null || array.length == 0) {
+			return false;
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				if (i == j || i % 2 == 1) continue;
+				if (array[i] == 2 * array[j] || array[j] == 2 * array[i]) {
 					return true;
 				}
 			}
