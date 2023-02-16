45d8876400049d9d2664ce2aa21151bdb4f5e76e
agmcoding <antonioibmachado@gmail.com>
2023-02-15 22:00:48 -0300

    Added one more solution for challenge ThirdMaximumNumber

--- ProjectJavaCodeChallenges/src/packageThirdMaximumNumber/Main.java
+++ ProjectJavaCodeChallenges/src/packageThirdMaximumNumber/Main.java
@@ -1,6 +1,10 @@
 package packageThirdMaximumNumber;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
@@ -45,34 +49,77 @@ public class Main {
 	
 	public static void main(String[] args) {
 
-		int[] nums = {2,2,3,1};
+		System.out.print("With a List: ");
+
+		List<Integer> nums = new ArrayList<>(Arrays.asList(2, 2, 3, 1));
+		System.out.println(MaxNumberByPositionWithList(nums, 3));
 		
-		System.out.println(thirdMax(nums));
+		System.out.print("Now with a primitive data type array: ");
+
+		int[] numsPrimitiveArray = { 2, 2, 3, 1};
+		System.out.println(MaxNumberByPositionWithPrimitiveArray(numsPrimitiveArray, 3));
+		
+		/**
+		 * With a List: 1
+		 * Now with a primitive data type array: 1
+		 */
 		
 	}
 
 	
-	private static int thirdMax(int[] nums) {
+	/**
+	 * Finds a distinct maximum integer number in a array, according to the position passed.
+	 * For finding the third distinct maximum number, pass maxWantedPosition as 3.
+	 * If the maxWantedPosition does not exist in the array, the maximum number is returned instead.
+	 */
+	private static int MaxNumberByPositionWithList(List<Integer> nums, int maxWantedPosition) {
+		
+		Optional<List<Integer>> maybeNullNums = Optional.ofNullable(nums);
+		if (maybeNullNums.isEmpty()) return -1;
+		
+		List<Integer> numsSorted = new ArrayList<>(nums);
+		Collections.sort(numsSorted); // all max values should be shifted to the right.
+		
+		int currentMaximumNumber = Collections.max(numsSorted);
+		int MaxNumbersFound = 1;
+		int index = numsSorted.size() - 2; // the index will start close to the end of the array and then decrease.
+		
+		while (MaxNumbersFound < maxWantedPosition && index >= 0) {
+			
+			if (numsSorted.get(index) < numsSorted.get(index + 1)) {
+				++MaxNumbersFound;
+				currentMaximumNumber = (MaxNumbersFound == maxWantedPosition) ? numsSorted.get(index) : currentMaximumNumber;
+			}
+				
+			index--;
+		}
+		
+		return currentMaximumNumber;
+	}
+
+
+
+	private static int MaxNumberByPositionWithPrimitiveArray(int[] nums, int maxWantedPosition) {
 		
 		if (nums == null || nums.length == 0) return -1;
         
-        Arrays.sort(nums); // all max values should be shifted to the right.
+		int[] numsSorted = nums.clone();
+        Arrays.sort(numsSorted); // all max values should be shifted to the right.
         
-        int maxValuesFound = 1;
-        /* the index will start close to the end of the array and then decrease. */
-        int index = nums.length - 2;
-        int currentMaxValue = nums[nums.length - 1];
+        int currentMaximumNumber = numsSorted[numsSorted.length - 1];
+        int MaxNumbersFound = 1;
+        int index = numsSorted.length - 2; // the index will start close to the end of the array and then decrease.
         
-        while (maxValuesFound < 3 && index >= 0) {
+        while (MaxNumbersFound < maxWantedPosition && index >= 0) {
             
-            if (nums[index] < nums[index + 1]) {
-                ++maxValuesFound;
-                currentMaxValue = maxValuesFound == 3 ? nums[index] : currentMaxValue;
+            if (numsSorted[index] < numsSorted[index + 1]) {
+                ++MaxNumbersFound;
+                currentMaximumNumber = (MaxNumbersFound == maxWantedPosition) ? numsSorted[index] : currentMaximumNumber;
             }
             
             index--;
         }
-        return currentMaxValue;
+        return currentMaximumNumber;
 	}
 
 }
