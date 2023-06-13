a51ab7af8b7b6860561c36903d04dbd4fb34a140
agmcoding <antonioibmachado@gmail.com>
2023-06-13 19:39:59 -0300

    update and add details

--- ProjectJavaCodeChallenges/src/packageHeightChecker/Main.java
+++ ProjectJavaCodeChallenges/src/packageHeightChecker/Main.java
@@ -2,9 +2,7 @@ package packageHeightChecker;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -56,17 +54,17 @@ public class Main {
 //	1 <= heights[i] <= 100
 	
 	public static void main(String[] args) {
-		
+
 		System.out.print("With a primitive data type array: ");
-		
-		int[] heights = {1,1,4,2,1,3};
+
+		int[] heights = { 1, 1, 4, 2, 1, 3 };
 		System.out.println(heightChecker(heights));
-		
+
 		System.out.print("\nWith a List: ");
-		
-		List<Integer> heights2 = new ArrayList<>(Arrays.asList(1,1,4,2,1,3));
+
+		List<Integer> heights2 = new ArrayList<>(Arrays.asList(1, 1, 4, 2, 1, 3));
 		System.out.println(heightCheckerWithList(heights2));
-		
+
 		/**
 		 * Output Result:
 		 * 
@@ -74,41 +72,64 @@ public class Main {
 		 * 
 		 * With a List: 3
 		 */
-		
+
 	}
 
 
-	
+
+	/**
+	 * Returns the number of indices where {@code heights[i] != expected[i]}. The
+	 * <b>expected[i]</b> array is the expected height of the ith student in line.
+	 * If <b>heights</b> is {@code null} or has a length smaller than 1 it returns a
+	 * -1 int. This could be at business definition discretion.
+	 * 
+	 * @param heights the integer array representing the current order that the
+	 *                students are standing in
+	 * @return the number of indices where {@code heights[i] != expected[i]}
+	 */
 	static int heightChecker(int[] heights) {
-		
-		if (heights == null || heights.length <= 1) return 0;
-		
-		int[] sortedHeights = heights.clone();
-		Arrays.sort(sortedHeights); // Sort primitive data types
-		int countHeightsInWrongPosition = 0;
-		
-		for (int i = 0; i < heights.length; i++) {
-			if (heights[i] != sortedHeights[i]) countHeightsInWrongPosition++;
+
+		if (heights == null || heights.length < 1) {
+			return -1; // At business definition discretion.
 		}
-		
+
+		int[] sortedHeights = heights.clone();
+		Arrays.sort(sortedHeights);
+		int countHeightsInWrongPosition = 0;
+
+		for (int i = 0; i < heights.length; i++) {
+			if (heights[i] != sortedHeights[i]) {
+				countHeightsInWrongPosition++;
+			}
+		}
+
 		return countHeightsInWrongPosition;
 	}
-	
-	
-	
+
+
+
+	/**
+	 * Returns the number of indices where {@code heights[i] != expected[i]}. The
+	 * <b>expected[i]</b> List is the expected height of the ith student in line. If
+	 * <b>heights</b> is {@code null} or has a size smaller than 1 it returns a -1
+	 * Long. This could be at business definition discretion.
+	 * 
+	 * @param heights the integer array representing the current order that the
+	 *                students are standing in
+	 * @return the number of indices where {@code heights[i] != expected[i]}
+	 */
 	static long heightCheckerWithList(List<Integer> heights) {
-		
-		Optional<List<Integer>> maybeNull = Optional.ofNullable(heights);
-		if (maybeNull.isEmpty() || heights.size() <= 1) return 0;
-		
-		List<Integer> sortedHeights = heights.stream().collect(Collectors.toList());
-		Collections.sort(sortedHeights);
-		
+
+		if (heights == null || heights.size() < 1) {
+			return -1L; // At business definition discretion.
+		}
+
+		List<Integer> sortedHeights = heights.stream().sorted().collect(Collectors.toList());
+
 		long countHeightsInWrongPosition = IntStream.range(0, heights.size())
-				.filter(index -> ! heights.get(index).equals(sortedHeights.get(index))).count();
-		
+				.filter(index -> !heights.get(index).equals(sortedHeights.get(index))).count();
+
 		return countHeightsInWrongPosition;
 	}
-	
-	
+
 }
